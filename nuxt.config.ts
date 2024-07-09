07d5cbde5f73ce9f1b949f7abdce99079487c96a
Alexander Frandsen <alex@jobindex.dk>
2024-07-09 17:37:36 +0200

    Set ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  app: { baseURL: "/pages/" }
+  app: { baseURL: "/pages/" },
+  ssr: false,
 })
